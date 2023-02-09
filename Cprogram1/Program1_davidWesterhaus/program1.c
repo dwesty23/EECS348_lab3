22d8d9f2ce928c26199cf768628b6b49f72337e8
dwesty23 <120140940+dwesty23@users.noreply.github.com>
2023-02-09 14:21:28 -0600

    fixing errors that show up in vs

--- Cprogram1/Program1_davidWesterhaus/program1.c
+++ Cprogram1/Program1_davidWesterhaus/program1.c
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 
 char* read_file(){
-	FILE *file_ptr
+	FILE *file_ptr;
 	float numbers_array[12];
 	char line[100];
 	int i = 0;
@@ -21,7 +21,7 @@ char* read_file(){
 }
 
 void sales_report(char* numbers, char*months){
-	i = 0
+	int i = 0;
 	
 	printf("Monthly Sales Report for 2022:\n");
 	printf("Month\tSales\n");
@@ -32,9 +32,9 @@ void sales_report(char* numbers, char*months){
 	
 }
 
-void sales_sum(char* numbers){
-	i = 0
-	int max = numbers[0]
+void sales_sum(char* numbers, char* months){
+	int i = 0;
+	int max = numbers[0];
 	int max_month;				  
     int min = numbers[0];
     int min_month;
@@ -44,24 +44,26 @@ void sales_sum(char* numbers){
 		if(numbers[i] > max){
 			max = numbers[i];
 			max_month = i;
-		}
+		}}
 	for(i=0; i<12; i++){
 			if(numbers[i] < min){
 				min = numbers[i];
 				min_month = i;
-			}
+			}}
 	float sum = numbers[0]+numbers[1]+numbers[2]+numbers[3]+numbers[4]+numbers[5]+numbers[6]+numbers[7]+numbers[8]+numbers[9]+numbers[10]+numbers[11];
 	float avg = sum/12;	
 	
 	printf("Minimum sales:\t%f\t%s\n", min, months[min_month]);
 	printf("Maximum sales:\t%f\t%s\n", max, months[max_month]);
 	printf("Average sales:\t%f" , avg);
-
+}
 
 
 int main(){
-	numbers = read_file();
-	char* months[12] = {"January", "February", "March", "April". "May", "June", "July", "August", "September", "October", "November", "December"};
-	
+	char* numbers = read_file();
+	char* months[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+
+
 }
 
