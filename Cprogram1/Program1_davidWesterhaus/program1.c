e0f1327c4b1a8ae3b4ff94f6697ee02685d744e6
dwesty23 <david.b.westerhaus@gmail.com>
2023-02-09 12:28:01 -0600

    doing work for 348

---
+++ Cprogram1/Program1_davidWesterhaus/program1.c
@@ -0,0 +1,67 @@
+/*
+ * program1.c
+ *
+ *  Created on: Feb 9, 2023
+ *      Author: d643w879
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+char* read_file(){
+	FILE *file_ptr
+	float numbers_array[12];
+	char line[100];
+	int i = 0;
+	file_ptr = fopen("input.txt" , "r");
+	
+	while(fgets(line, 100, file_ptr)){
+		numbers_array[i] = atof(line);
+		i++;
+	} return numbers_array;
+}
+
+void sales_report(char* numbers, char*months){
+	i = 0
+	
+	printf("Monthly Sales Report for 2022:\n");
+	printf("Month\tSales\n");
+	printf("\n");
+	for(i=0; i<12; i++){
+		printf("months[i] \t numbers[i]");
+	}
+	
+}
+
+void sales_sum(char* numbers){
+	i = 0
+	int max = numbers[0]
+	int max_month;				  
+    int min = numbers[0];
+    int min_month;
+	printf("Sales Summary:\n");
+	
+	for(i=0; i<12; i++){
+		if(numbers[i] > max){
+			max = numbers[i];
+			max_month = i;
+		}
+	for(i=0; i<12; i++){
+			if(numbers[i] < min){
+				min = numbers[i];
+				min_month = i;
+			}
+	float sum = numbers[0]+numbers[1]+numbers[2]+numbers[3]+numbers[4]+numbers[5]+numbers[6]+numbers[7]+numbers[8]+numbers[9]+numbers[10]+numbers[11];
+	float avg = sum/12;	
+	
+	printf("Minimum sales:\t%f\t%s\n", min, months[min_month]);
+	printf("Maximum sales:\t%f\t%s\n", max, months[max_month]);
+	printf("Average sales:\t%f" , avg);
+
+
+
+int main(){
+	numbers = read_file();
+	char* months[12] = {"January", "February", "March", "April". "May", "June", "July", "August", "September", "October", "November", "December"};
+	
+}
+
