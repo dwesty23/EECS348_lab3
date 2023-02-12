3a14997d0d6ba36a8cdf1876422e0eed5f485edf
dwesty23 <120140940+dwesty23@users.noreply.github.com>
2023-02-12 12:43:31 -0600

    fixing program 2 to include td

--- Cprogram2/program2.c
+++ Cprogram2/program2.c
@@ -9,11 +9,12 @@
 #include <string.h>
 
 void combos(int score){
-    int td2, td1, fg, saf, n;
+    int td2, td1, td, fg, saf, n;
     /*
     score -> user input to take score
     td2 -> TOUCHDOWN with 2-PT CONVERSION (8 points)
     td1 -> TOUCHDOWN with EXTRA POINT (7 points)
+    td -> TOUCHDOWN (6 points)
     fg -> FIELD GOAL (3 points)
     saf -> SAFETY (2 points)
     n -> leftover score
@@ -22,11 +23,12 @@ void combos(int score){
     printf("Possible combinations of scoring plays for %d:\n", score);
     for (td2 = 0; td2 <= score/8; td2++) {
         for (td1 = 0; td1 <= (score-8*td2)/7; td1++) {
-            for (fg = 0; fg <= (score-6*td2-7*td1)/3; fg++) {
-                for (saf = 0; saf <= (score-8*td2-7*td1-3*fg)/2; saf++) {
-                        n = score - 8*td2 - 7*td1 - 3*fg - 2*saf;
+            for (td = 0; td <= (score-8*td2-7*td1)/6; td++)
+            for (fg = 0; fg <= (score-6*td2-7*td1-6*td)/3; fg++) {
+                for (saf = 0; saf <= (score-8*td2-7*td1-6*td-3*fg)/2; saf++) {
+                        n = score - 8*td2 - 7*td1 -6*td - 3*fg - 2*saf;
                         if (n == 0) {
-                            printf("%d touchdowns(with 2-point conversion), %d touchdowns(with extra point), %d field goals, %d safeties\n", td2, td1, fg, saf);
+                            printf("%d touchdowns(with 2-point conversion), %d touchdowns(with extra point), %d touchdowns, %d field goals, %d safeties\n", td2, td1, td, fg, saf);
                         }
                     }
                 }
